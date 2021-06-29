4bdb1a11a798514fd8ada42f6825200c9c2cbd91
Elvis-Aef <ea.espinoza.f@gmail.com>
2021-06-29 13:06:48 -0400

    Creación de la clase Dispositivo Entrada

---
+++ dispositivo_entrada.py
@@ -0,0 +1,17 @@
+#Section 18: Polimorfismo.
+#Leccion 01: Mundo PC (Clase Dispositivo).
+
+'''
+Nota:
+'''
+
+class DispositivoEntrada(object):
+	"""docstring for DispositivoEntrada"""
+	def __init__(self, marca, tipo_entrada):
+		self._marca = marca
+		self._tipo_entrada = tipo_entrada 
+		
+
+	
+	
+		
